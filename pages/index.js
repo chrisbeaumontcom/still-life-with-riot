8afd750e0de502fef74b6e0017dd647ac7ca13d2
Chris Beaumont <chris@chrisbeaumont.com>
2021-03-11 19:56:14 +1100

    Add basic colour scheme

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <h1 className="title">
           <img
             className="resp"
-            src="https://res.cloudinary.com/web-school/image/upload/w_600,q_auto:best/still-life-with-riot/still-life-with-riot-ed_zyejwy.jpg"
+            src="https://res.cloudinary.com/web-school/image/upload/w_800,q_auto:best/still-life-with-riot/still-life-with-riot-ed_amku5w.jpg"
             alt="Still Life with Riot"
           />
           <br />
@@ -21,6 +21,66 @@ export default function Home() {
         </h1>
 
         <p className="description">Oil on linen, 61 x 76cm, 2021</p>
+        <div className="blurb">
+          <p>2021 is off and racing... and started with a riot!</p>
+
+          <p>
+            This painting is an effort at History painting - even if it is a
+            still life.{' '}
+          </p>
+
+          <p>
+            When I was setting prices with the gallery for a recent exhibition,
+            the gallery told me we could only ask so much for still life
+            paintings because that's what the market will bear. So I'm getting
+            all genre-d up here and going for some HISTORY genre stuff. (FYI:
+            Paintings are also priced by square centimeter like wall-paper or
+            pizza.)
+          </p>
+
+          <p>
+            This work is about time. Paintings are painted over time unlike
+            photographs which are records of an instant. Both are images but
+            paintings really are things, and the recording they really make is
+            of their own execution.
+          </p>
+
+          <p>
+            I wanted to play with the performance of an oil painting like a
+            composer creating variations on a theme and a musician performing
+            the same piece multiple times. I set out to take a previous
+            composition and execute/perform it in three variations: the same
+            still life subject arrangement with three completely different
+            visual contexts.
+          </p>
+
+          <p>
+            This one is the third in a series of three identical still life
+            arrangements of lemons on a marble ledge. The first has an entirely
+            black background with hints of geometry. The second has a colourful
+            geometric field as a background. This third one jumps the shark
+            (relatively) into history and was originally painted as a grisaille
+            with no reference to riots. That version is now history because I
+            have completely over-painted it in glorious technicolor including a
+            social media driven riot that will certainly go down in US and world
+            history.
+          </p>
+
+          <p>
+            History is a race to what happened next. The horse and jockey is
+            borrowed from a past commission and surrealistically challenges the
+            stasis of the still life of lemons.
+          </p>
+
+          <p>
+            This picture uses the (very melbourne) neo-classical architecture in
+            the upper left of Tiepolo's The Banquet of Cleopatra, a painting
+            about an historical moment. When I saw the images on social media of
+            the US Capitol riots of 6 January I knew I had to re-paint Tieplo's
+            balconies. They had changed role from passive witness to active
+            insurrection.
+          </p>
+        </div>
 
         <div className="grid">
           <a
@@ -39,10 +99,22 @@ export default function Home() {
           </a>
 
           <a
+            href="https://www.buzzfeednews.com/article/elaminabdelmahmoud/trump-mob-social-media-insurrection"
+            className="card"
+          >
+            <h3>The Pro-Trump Mob Was Doing It For The ’Gram</h3>
+            <p>
+              Elamin Abdelmahmoud
+              <br />
+              Buzzfeed
+            </p>
+          </a>
+
+          <a
             href="https://www.theatlantic.com/magazine/archive/2021/04/the-internet-doesnt-have-to-be-awful/618079/"
             className="card"
           >
-            <h3>How to Put Out Democracy’s Dumpster Fire</h3>
+            <h3>How to Put Out Democracy's Dumpster Fire</h3>
             <p>Anne Applebaum and Peter Pomerantsev - The Atlantic</p>
           </a>
 
@@ -56,6 +128,16 @@ export default function Home() {
               <br /> Medium
             </p>
           </a>
+          <a
+            href="https://www.youtube.com/watch?v=-6rWqJhDv7M"
+            className="card"
+          >
+            <h3>The Physics and Philosophy of Time</h3>
+            <p>
+              Carlo Rovelli
+              <br /> YouTube
+            </p>
+          </a>
         </div>
       </main>
 
@@ -65,8 +147,9 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Published by{' '}
+          Published by
           <img src="/cb-site.png" alt="Site Logo" title="Chris Beaumont" />
+          chrisbeaumont.com
         </a>
       </footer>
 
@@ -74,8 +157,12 @@ export default function Home() {
         img.resp {
           width: 100%;
           height: auto;
+          border: 10px solid #5a989e;
+          outline: 1px solid #eeeeee;
         }
         .container {
+          background-color: #9dc6c2;
+          color: #ffffff;
           min-height: 100vh;
           padding: 0 0.5rem;
           display: flex;
@@ -93,6 +180,16 @@ export default function Home() {
           align-items: center;
         }
 
+        .blurb {
+          display: flex;
+          align-items: left;
+          justify-content: left;
+          flex-wrap: wrap;
+
+          max-width: 800px;
+          margin-top: 1rem;
+        }
+
         footer {
           width: 100%;
           height: 100px;
@@ -104,6 +201,7 @@ export default function Home() {
 
         footer img {
           margin-left: 0.5rem;
+          margin-right: 0.5rem;
         }
 
         footer a {
@@ -131,7 +229,7 @@ export default function Home() {
         .title {
           margin: 0;
           line-height: 1.15;
-          font-size: 4rem;
+          font-size: 3rem;
         }
 
         .title,
@@ -173,6 +271,7 @@ export default function Home() {
           border: 1px solid #eaeaea;
           border-radius: 10px;
           transition: color 0.15s ease, border-color 0.15s ease;
+          box-shadow: 3px 3px 3px 3px #83aea3;
         }
 
         .card:hover,
@@ -194,6 +293,13 @@ export default function Home() {
         }
 
         @media (max-width: 600px) {
+          .title {
+            font-size: 2rem;
+          }
+          .description {
+            line-height: 1;
+            font-size: 1.1rem;
+          }
           .grid {
             width: 100%;
             flex-direction: column;
